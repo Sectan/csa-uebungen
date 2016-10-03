6f1dab557fadd5e33748d1c78e786082586e704c
Sectan <daniel.roesch@stud.hslu.ch>
2016-10-03 14:12:07 +0200

    add Hello World project

---
+++ csa-uebungen/ueb01-hello-world/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ueb01_hello_world
+{
+    class HelloWorld
+    {
+        private void PrintMessage(string msg, bool upperCase)
+        {
+            if(upperCase)
+            {
+                msg = msg.ToUpper();
+            }
+            Console.WriteLine(msg);
+        }
+        static void Main(string[] args)
+        {
+            HelloWorld helloWorld = new HelloWorld();
+
+            helloWorld.PrintMessage("Hello World", false);
+            helloWorld.PrintMessage("Hello World", true);
+
+            Console.ReadLine();
+        }
+    }
+}
